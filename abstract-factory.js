58d9ed8ec648f8c29024524b572d2e19264c914a
Alexander Kasheverov <alexander.kasheverov@dataart.com>
2020-03-03 07:30:37 +0300

    abstract factory

--- abstract-factory.js
+++ abstract-factory.js
@@ -1,49 +1,58 @@
-
-(function() {
-
-    var MockWidgetFactory = function(widget) {
-        var CLASS_PREF = 'MockWidget_';
-        return new this[CLASS_PREF + widget]();
-    };
-
-    var WidgetFactory = function(widget) {
-        var CLASS_PREF = 'Widget_';
-        return new this[CLASS_PREF + widget]();
-    };
-
-    // Abstract
-    var Factory = (function() {
-        var _widgetFactory = WidgetFactory;
-        return {
-            setFactory: function(factory) {
-                _widgetFactory = factory;
-            },
-            widget: function(widget) {
-                var instance = _widgetFactory(widget);
-                instance.init();
-                return instance;
-            }
+function CarFactory() {}
+CarFactory.prototype.createCar = function (model) {
+    var car;
+    switch(model) {
+        case 'Cayman':
+            car = new Cayman();
+            break;
+        case 'Boxster':
+            car = new Boxster();
+            break;
+        default:
+            car = new Cayman();
+            break;
+    }
+    if (typeof car.printModel === 'undefined') {
+        car.printModel = function () {
+            console.log('This car model is:', car.model);
         }
-    }());
+    }
+    return car;
+};
 
-    Widget_Slideshow = function() {
-        return {
-            init: function() {
-                console.log('Widget slideshow initialized');
-            }
-        }
-    };
-    MockWidget_Slideshow = function() {
-        return {
-            init: function() {
-                console.log('Mock widget slideshow initialized');
-            }
-        }
-    };
+function Cayman() { this.model = 'Cayman'; }
+Cayman.prototype.createDoor = function (side) { return CaymanDoor(side); };
+Cayman.prototype.createHood = function () { return CaymanHood(); };
+function CaymanDoor(side) {
+    return {
+        build: function() { console.log(`Build a ${side} door for Cayman`); }
+    }
+}
+function CaymanHood() {
+    return {
+        build: function() { console.log(`Build a hood for Cayman`); }
+    }
+}
 
+function Boxster() { this.model = 'Boxster'; }
+Boxster.prototype.createDoor = function (side) { return BoxsterDoor(side); };
+Boxster.prototype.createHood = function () { return BoxsterHood(); };
+function BoxsterDoor(side) {
+    return {
+        build: function() { console.log(`Build a ${side} door for Boxster`); }
+    }
+}
+function BoxsterHood() {
+    return {
+        build: function() { console.log(`Build a hood for Boxster`); }
+    }
+}
 
-    var widget = Factory.widget('Slideshow');
-    Factory.setFactory(MockWidgetFactory);
-    var widget2 = Factory.widget('Slideshow');
+var carFactory = new CarFactory();
 
-}());
+var boxsterCar = carFactory.createCar('Boxster');
+
+boxsterCar.printModel();
+boxsterCar.createDoor('right').build();
+boxsterCar.createDoor('left').build();
+boxsterCar.createHood().build();
